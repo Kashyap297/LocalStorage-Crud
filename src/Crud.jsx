a9b064137bb91f2ac492dd6d83f1cf7832920071
Kashyap297 <kashyap29700@gmail.com>
2023-12-17 23:00:58 +0530

    Final Proj. done

--- src/Crud.jsx
+++ src/Crud.jsx
@@ -1,17 +1,25 @@
 import React, { useEffect, useState } from 'react'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
-import Table from './Table';
-
 
 const Crud = () => {
     const initialValue = {
         name: "",
-        email: ""
+        email: "",
+        address: ""
     }
     const [input, setInput] = useState(initialValue)
-    const [datas, setDatas] = useState([])
+    const [datas, setDatas] = useState(() => {
+        return JSON.parse(localStorage.getItem('users')) || [];
+    })
     const [errors, setErrors] = useState({})
+    const [edit, setEdit] = useState(false)
+    const [editIndex, setEditIndex] = useState(null)
+    const [button, setButton] = useState(false)
+
+    useEffect(() =>{
+        localStorage.setItem('users', JSON.stringify(datas))
+    }, [datas])
 
     const handleChange = (e) => {
         const key = e.target.name
@@ -19,11 +27,6 @@ const Crud = () => {
 
         setInput({ ...input, [key]: value })
     }
-
-    useEffect(() => {
-        // localStorage.setItem('data', JSON.stringify(datas));
-        localStorage.setItem('data', JSON.stringify(datas));
-    }, [datas]);
 
     const checkValidate = (input) => {
         const errors = {}
@@ -34,6 +37,9 @@ const Crud = () => {
 
         if (input.email === "" || input.email === " ") {
             errors.email = "Invalid E-mail*"
+        }
+        if (input.address === "" || input.address === " ") {
+            errors.address = "Invalid Address*"
         }
         return errors
     }
@@ -49,7 +55,30 @@ const Crud = () => {
             setDatas([...datas, input])
             setInput(initialValue)
         }
+        if(edit){
+            datas[editIndex] = input;
+            setDatas(datas)
+            setEdit(false)
+            
+        }
+        setButton(false)
+    }
 
+    const handleDelete = (index) => {
+        // console.log(index);
+        const temp = [...datas]
+        temp.splice(index, 1)
+
+        setDatas(temp)
+    }
+
+    const handleEdit = (index) => {
+        setEdit(true)
+        const editData = [...datas]
+        editData.splice(index, 1)
+        setInput(datas[index])
+        setEditIndex(index)
+        setButton(true)
     }
 
     return (
@@ -71,14 +100,49 @@ const Crud = () => {
                                         <input type="email" className='control-form' name="email" id="" placeholder='Enter Email ID' value={input.email} onChange={handleChange} />
                                     </div>
                                     <div className="text-danger text-end fw-bold">{errors && errors.email}</div>
-                                    <button className='control-form mt-3'>Submit</button>
+                                    <div className="d-flex align-items-center justify-content-center mt-3">
+                                        <i className="ri-map-pin-line me-3"></i>
+                                        <input type="text" className='control-form' name="address" id="" placeholder='Enter Address' value={input.address} onChange={handleChange} />
+                                    </div>
+                                    <div className="text-danger text-end fw-bold">{errors && errors.address}</div>
+                                    <button type='submit' className='control-form mt-3'>{button ? "Update" : "Submit"}</button>
                                 </form>
                             </div>
                         </div>
                     </div>
                 </div>
             </section>
-            <Table datas={datas} setDatas={setDatas} />
+            <section className="table-data my-5">
+                <div className="container">
+                    <h2 className='text-center text-white'>Details</h2>
+                    <div className="col-8 m-auto">
+                        <table border={1} className='table table-bordered table-striped mt-4' id='resultsTable'>
+                            <thead className='text-center'>
+                                <tr>
+                                    <th className='col-3'>Name</th>
+                                    <th className='col-3'>Email</th>
+                                    <th className='col-3'>Address</th>
+                                    <th className='col-3'>Action</th>
+                                </tr>
+                            </thead>
+                            <tbody className='text-center'>
+                                {
+                                    datas.map((data, index) => {
+                                        return (
+                                            <tr key={index}>
+                                                <td>{data.name}</td>
+                                                <td>{data.email}</td>
+                                                <td>{data.address}</td>
+                                                <td><button className='edit-btn me-2' onClick={() => handleEdit(index)}>Edit</button> <button className='dlt-btn' onClick={() => handleDelete(index)}>Delete</button></td>
+                                            </tr>
+                                        )
+                                    })
+                                }
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </section >
         </>
     )
 }
